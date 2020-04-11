b62a1c473886028ed80d56b55da4373e460f7dc2
Rohith <rohithcholleti97@gmail.com>
2020-04-11 11:29:12 +0530

    Core Default Application

---
+++ CoreWebApplication1/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace CoreWebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _config;
+
+        public IndexModel(IConfiguration config, ILogger<IndexModel> logger)
+        {
+            _config = config;
+            _logger = logger;
+        }
+
+        public string msg = "hello world!";
+        public void OnGet()
+        {
+
+            msg = _config["Message"];
+        }
+    }
+}
